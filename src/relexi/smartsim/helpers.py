ef236d7099e21a3dac54f23475105fef0da2e07a
Marius Kurz <marius.kurz@iag.uni-stuttgart.de>
2024-06-03 08:58:58 +0200

    Apply PEP8 style to SmartSim helper module

--- src/relexi/smartsim/helpers.py
+++ src/relexi/smartsim/helpers.py
@@ -1,146 +1,175 @@
 #!/usr/bin/env python3
 
+"""Contains helper functions for managing simulations on HPC systems.
+
+This module implements several auxiliary functions to manage simulation
+instances on HPC systems. This contains in particular functions to generate
+rankfiles for OpenMPI for correct mappings and to move simulation files to the
+worker nodes for faster startup.
+"""
+
 import os
 import re
 import glob
 import shutil
 
 
-def generate_rankfile_ompi(hosts: list, cores_per_node: int, n_par_env: int, ranks_per_env: int, base_path=None):
-  """Generate rank file for openmpi process binding
-  :param host: list of hosts
-  :type host: list[str]
-  :param cores_per_node: number of cores per node
-  :type cores_per_node: int
-  :param n_par_env: number of parallel environments
-  :type n_par_env: int
-  :param ranks_per_env: number of ranks per environments
-  :type ranks_per_env: int
-  :param base_path: path to the base directory of the rank files
-  :type base_path: str
-  """
+def generate_rankfile_ompi(hosts, cores_per_node, n_par_env, ranks_per_env, base_path=None):
+    """Generate rank file for OpenMPI process binding.
 
-  # If no base_path given, use CWD
-  if base_path:
-    rankfile_dir = os.path.join(base_path, "ompi-rankfiles")
-  else:
-    rankfile_dir = "ompi-rankfiles"
+    Args: 
+        hosts (list): List of hostnames
+        cores_per_node (int): Number of cores per node
+        n_par_env (int): Number of parallel environments to be launched
+        ranks_per_env (int): Number of ranks per environments
+        base_path (str): (Optional.) Path to the directory of the rank files
 
-  if os.path.exists(rankfile_dir):
-    shutil.rmtree(rankfile_dir)
-  os.makedirs(rankfile_dir, exist_ok=True)
+    Returns:
+        list: List of filenames of the rankfiles
+    """
 
-  rankfiles = list()
-  next_free_slot = 0
-  n_cores_used = 0
-  for env_idx in range(n_par_env):
-    filename = os.path.join(rankfile_dir, f"par_env_{env_idx:05d}")
-    rankfiles.append(filename)
-    with open(filename, 'w') as rankfile:
-      for i in range(ranks_per_env):
-        rankfile.write(f"rank {i}={hosts[n_cores_used // cores_per_node]} slot={next_free_slot}\n")
-        next_free_slot = next_free_slot + 1
-        n_cores_used = n_cores_used + 1
+    # If no base_path given, use CWD
+    if base_path:
+        rankfile_dir = os.path.join(base_path, "ompi-rankfiles")
+    else:
+        rankfile_dir = "ompi-rankfiles"
 
-        if next_free_slot > ( cores_per_node - 1 ):
-          next_free_slot = 0
+    if os.path.exists(rankfile_dir):
+        shutil.rmtree(rankfile_dir)
+    os.makedirs(rankfile_dir, exist_ok=True)
 
-    files = os.listdir(rankfile_dir)
-
-  return rankfiles
+    rankfiles = []
+    next_free_slot = 0
+    n_cores_used = 0
+    for env_idx in range(n_par_env):
+        filename = os.path.join(rankfile_dir, f"par_env_{env_idx:05d}")
+        rankfiles.append(filename)
+        with open(filename, 'w', encoding='ascii') as rankfile:
+            for i in range(ranks_per_env):
+                rankfile.write(f"rank {i}={hosts[n_cores_used//cores_per_node]} slot={next_free_slot}\n")
+                next_free_slot = next_free_slot + 1
+                n_cores_used = n_cores_used + 1
+                if next_free_slot > (cores_per_node - 1):
+                    next_free_slot = 0
+    return rankfiles
 
 
-def parser_flexi_parameters(parameter_file, keyword, new_keyword_value):
-  """
-  """
+def parser_flexi_parameters(parameter_file, keyword, value):
+    """Changes the value for a keyword in a FLEXI parameter file.
 
-  pattern = re.compile(r"(%s)\s*=.*" % keyword, re.IGNORECASE)
-  subst = keyword + "=" + new_keyword_value
-  parameter_file_in = parameter_file
-  pbs_jobID = os.environ['PBS_JOBID']
-  parameter_file_out = "parameter_flexi-" + pbs_jobID[0:7] + ".ini"
+    The FLEXI parameter file is structured as in the form of
+        `keyword = value`
+    This function changes the `value` for a given keyword using regular
+    expressions and writes the modified contents to a new file.
 
-  with open(parameter_file_out,'w') as new_file:
-    with open(parameter_file_in, 'r') as old_file:
-      for line in old_file:
-        new_file.write(pattern.sub(subst, line))
+    Args:
+        parameter_file (str): Path to FLEXI parameterfile.
+        keyword (str): Keyword of the parameter.
+        value (str): New value for the given keyword.
 
-  return parameter_file_out
+    Returns:
+        str: Path to new (modified) parameter file
+    """
+    pattern = re.compile(fr"({keyword})\s*=.*", re.IGNORECASE)
+    subst = keyword + "=" + value
+    parameter_file_in = parameter_file
+    pbs_job_id = os.environ['PBS_JOBID']
+    parameter_file_out = f"parameter_flexi-{pbs_job_id[0:7]}.ini"
+
+    with open(parameter_file_out, 'w', encoding='ascii') as new_file:
+        with open(parameter_file_in, 'r', encoding='ascii') as old_file:
+            for line in old_file:
+                new_file.write(pattern.sub(subst, line))
+    return parameter_file_out
 
 
-def clean_ompi_tmpfiles():
-  """
-  Cleans up temporary files which are created by openmpi in TMPDIR
-  Avoids running out of space in TMPDIR
-  If TMPDIR is not found exists with -1 status
-  """
-  try:
-    tmpdir = os.environ['TMPDIR']
-  except:
-    return -1
+def clean_ompi_tmpfiles(env_variable="TMPDIR"):
+    """Cleans up temporary files created by OpenMPI.
 
-  path = os.path.join(tmpdir,'ompi.*')
-  path = glob.glob(path)
+    OpenMPI creates temporary files with each invocation, which might cause the
+    system to run out of disk space at some point. This routine tries to locate
+    the current folder for temporary files by evaluating the env variable
+    $TMPDIR and tries to remove all OpenMPI-specific files it finds.
 
-  for folder in path:
-    for filename in os.listdir(folder):
-      file_path = os.path.join(folder, filename)
-      if os.path.isdir(file_path):
-        shutil.rmtree(file_path)
+    Args:
+        env_variable (string): Name for environment variable where current
+            folder for termporary files is stored.
 
+    Returns:
+        int:
+            * 1 if operation was successfull,
+            * -1 otherwise.
+    """
+    try:
+        tmpdir = os.environ[env_variable]
+    except RuntimeError:
+        return -1
+
+    path = os.path.join(tmpdir, 'ompi.*')
+    path = glob.glob(path)
+
+    for folder in path:
+        for filename in os.listdir(folder):
+            file_path = os.path.join(folder, filename)
+            if os.path.isdir(file_path):
+                try:
+                    shutil.rmtree(file_path)
+                except RuntimeError:
+                    return -1
+    return 1
 
 
 def copy_to_nodes(my_files, base_path, hosts, subfolder=None):
-  """Copy files to local disk storage on allocated nodes.
+    """Copy files to local disk storage on allocated nodes.
 
-  This routine takes the files given in `my_files` and copies them
-  to `base_path` on the ssh targets `hosts` via the `scp` command.
-  If the path does not exists, it tries to create it via `mkdir`.
-  An optional `subfolder` can be given, which will be appended to
-  the `base_path`.
+    This routine takes the files given in `my_files` and copies them to
+    `base_path` on the ssh targets `hosts` via the `scp` command. If the path
+    does not exists, it tries to create it via `mkdir`. An optional `subfolder`
+    can be given, which will be appended to the `base_path`.
 
-  Args:
-    my_files (list): List of path names to the files that are copied.
-    base_path (list): Folder on the hosts to which the files should be copied.
-    hosts (list): List of hosts that are the ssh targets.
-    subfolder (str): (Optional.)
+    Args:
+        my_files (list): List of path names to the files that are copied.
+        base_path (list): Folder on hosts to which the files should be copied.
+        hosts (list): List of hosts that are the ssh targets.
+        subfolder (str): (Optional.)
 
-  Return:
-    list: list of paths to files on hosts
+    Return:
+        list: List of paths to files on hosts.
 
-  TODO:
-    Implement a fail-safe, i.e. only overwite filepaths for which copying worked.
-  """
+    TODO:
+        Implement a fail-safe: only overwrite filepaths for which copying
+        worked.
+    """
 
-  # If input not a list, i.e. a single element, transform into list
-  if isinstance(my_files, list):
-    conv_to_list = False
-  else:
-    my_files = [my_files]
-    conv_to_list = True
+    # If input not a list, i.e. a single element, transform into list
+    if isinstance(my_files, list):
+        conv_to_list = False
+    else:
+        my_files = [my_files]
+        conv_to_list = True
 
-  # Append subfolder if given
-  if subfolder:
-    target = os.path.join(base_path, subfolder)
-  else:
-    target = base_path
+    # Append subfolder if given
+    if subfolder:
+        target = os.path.join(base_path, subfolder)
+    else:
+        target = base_path
 
-  # Copy to all given hosts
-  for host in hosts:
-    # Create folder if necessary
-    os.system('ssh %s mkdir -p %s' % (host, target))
-    # Copy files
+    # Copy to all given hosts
+    for host in hosts:
+        # Create folder if necessary
+        os.system(f'ssh {host} mkdir -p {target}')
+        # Copy files
+        for my_file in my_files:
+            os.system(f'scp -q "{my_file}" "{host}:{target}"')
+
+    # Get new path of files
+    my_files_new = []
     for my_file in my_files:
-      os.system('scp -q "%s" "%s:%s"' % (my_file, host, target))
+        file_name = os.path.split(my_file)[1]
+        my_files_new.append(os.path.join(target, file_name))
 
-  # Get new path of files
-  my_files_new = []
-  for my_file in my_files:
-    file_name = os.path.split(my_file)[1]
-    my_files_new.append(os.path.join(target,file_name))
+    # Convert back to single string if input is single string
+    if conv_to_list:
+        my_files_new = my_files_new[0]
 
-  # Convert back to single string if input is single string
-  if conv_to_list:
-    my_files_new = my_files_new[0]
-
-  return my_files_new
+    return my_files_new
--- src/relexi/smartsim/init_smartsim.py
+++ src/relexi/smartsim/init_smartsim.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+"""Helpers for launching the SmartSim Orchestrator."""
+
 import os
 import json
 import socket
@@ -12,161 +14,188 @@ import relexi.io.output as rlxout
 
 
 def get_host():
-  """
-  Get the host the script is executed on from the env variable
-  """
-  return  socket.gethostname()
+    """Get the host the script is executed on from the env variable.
+
+    Returns:
+        Hostname as string
+    """
+    return socket.gethostname()
 
 
 def get_pbs_hosts():
-  """
-  Get the host list from the PBS Nodefile
-  """
-  nodefile_path = os.environ["PBS_NODEFILE"]
+    """Get the host list from the PBS Nodefile.
 
-  with open(nodefile_path, "r") as f:
-    hostlist = []
-    for line in f:
-      # only take the name not the entire ip-address otherwise there will be an error
-      # it will set the command line flag "mpirun ... -host <hostname_here>"
-      # This only works with the hostname shorthand
-      full_host_ip = line.strip()             # e.g. abc.ib0...de
-      hostname = full_host_ip.split(".")[0]   # e.g. abc
-      if not (hostname in hostlist):
-        hostlist.append(hostname)
-
-  return hostlist
+    Returns:
+        List containing the hostnames as strings
+    """
+    nodefile_path = os.environ["PBS_NODEFILE"]
+    with open(nodefile_path, "r", encoding='ascii') as f:
+        hostlist = []
+        for line in f:
+            # only take the name not the entire ip-address otherwise there will be an error
+            # it will set the command line flag "mpirun ... -host <hostname_here>"
+            # This only works with the hostname shorthand
+            full_host_ip = line.strip()             # e.g. abc.ib0...de
+            hostname = full_host_ip.split(".")[0]   # e.g. abc
+            if not hostname in hostlist:
+                hostlist.append(hostname)
+    return hostlist
 
 
 def get_pbs_walltime():
-  """
-  Get the walltime of the current PBS job
-  """
-  job_id = os.environ["PBS_JOBID"]
-  cmd=f"qstat -xfF json {job_id}"
-  stat_json_str = subprocess.check_output(cmd, shell=True, text=True)
-  stat_json = json.loads(stat_json_str)
+    """Get the walltime of the current PBS job.
 
-  return stat_json["Jobs"][job_id]["Resource_List"]["walltime"]
-
-
-
-def init_smartsim(port = 6790
-                 ,num_dbs = 1
-                 ,NETWORK_INTERFACE="ib0"
-                 ,launcher_type = "local"
-                 ,orchestrator_type = "local"
-                 ):
-  """
-  Initializes the smartsim architecture by starting the orchestrator, launching the experiment and get the list of hosts.
-
-  NOTE 1:
-    Combinations of Experiment launcher and orchestrator type:
-    (TL;DR: Must be identical!)
-    1. laun.: local, orch.: pbs   = incompatible
-    2. laun.: local, orch.: local = only one in memory database possible, mpirun will still distribute the flexi instances to other nodes
-    3. laun.: pbs,   orch.: pbs   = doesnt support clusters of size 2 otherwise works flawlessly (warning: orchestrator doesn't find the cluster configuration)
-    4. laun.: pbs,   orch.: local = not supported error: not supported by PBSPro  
-  """
-
-  rlxout.small_banner('Starting SmartSim...')
-
-  # Check whether launcher and orchestrator are identical (case-insensitive)
-  if not (launcher_type.casefold() == orchestrator_type.casefold()):
-    rlxout.warning('Chosen Launcher '+launcher_type+' and orchestrator '+orchestrator_type +' are incompatible! Please choose identical types for both!')
-
-  # Is database clustered, i.e. hosted on different nodes?
-  db_is_clustered = num_dbs > 1
-
-  # First try PBS if necessary. Use local configuration as backup
-  if launcher_type.casefold() == 'pbs':
-    PBS_failed = False
-    try:
-      # try to load the batch settings from the batch job environment variables like PBS_JOBID and PBS_NODEFILE
-      walltime = get_pbs_walltime()
-      hosts = get_pbs_hosts()
-      num_hosts = len(hosts)
-      rlxout.info(f"Identified available nodes: {hosts}")
-
-      # Maximum of 1 DB per node allowed for PBS Orchestrator
-      if num_hosts < num_dbs:
-        rlxout.warning(f"You selected {num_dbs} databases and {num_hosts} nodes, but maximum is 1 database per node. "+
-                      "Setting number of databases to {num_hosts}")
-        num_dbs = num_hosts
-
-      # Clustered DB with PBS orchestrator requires at least 3 nodes for reasons
-      if db_is_clustered:
-        if num_dbs < 3:
-          rlxout.warning(f"Only {num_dbs} databases requested, but clustered orchestrator requires 3 or more databases. "+
-                        "Non-clustered orchestrator is launched instead!")
-          db_is_clustered = False
-        else:
-          rlxout.info(f"Using a clustered database with {num_dbs} instances.")
-      else:
-        rlxout.info(f"Using an UNclustered database on root node.")
-
-    except:
-      # If there are no environment variables for a batchjob, then use the local launcher
-      rlxout.warning(f"Didn't find pbs batch environment. Switching to local setup.")
-      PBS_failed = True
+    Returns:
+       Walltime of current PBS job.
+    """
+    job_id = os.environ["PBS_JOBID"]
+    cmd = f"qstat -xfF json {job_id}"
+    stat_json_str = subprocess.check_output(cmd, shell=True, text=True)
+    stat_json = json.loads(stat_json_str)
+    return stat_json["Jobs"][job_id]["Resource_List"]["walltime"]
 
 
-  # If local configuration is required or if scheduler-based launcher failed.
-  if (launcher_type.casefold() == 'local') or PBS_failed:
-    launcher_type ="local"
-    orchestrator_type = "local"
-    db_is_clustered=False
-    hosts = [get_host()]
+def init_smartsim(
+    port=6790,
+    num_dbs=1,
+    network_interface="ib0",
+    launcher_type="local",
+    orchestrator_type="local"
+):
+    """Starts the orchestrator, launches an experiment and gets list of hosts.
 
+    Args:
+        port (int): (Optional.) Port number on which Orchestrator will be
+            launched.
+        num_dbs (int): (Optional.) Number of databases should be launched.
+            `num_dbs>1` imply that the database is clustered , i.e. distributed
+            across multiple instances.
+        network_interface (string) = (Optional.) Name of network interface to
+            be used to establish communication to clients.
+        launcher_type (string): (Optional.) Launcher to be used to start the
+            executable. Currently implemented are:
+                * local
+                * mpirun
+        orchestrator_type (string): Scheduler environment in which the
+            orchestrator is launched. Currently implemented are:
+                * local
+                * pbs
+    Returns:
+        smartsim.Experiment: The experiments in which the Orchestrator was
+            started
+        list: List of names of the nodes used as workers to run the simulations
+        smarsim.Orchestrator: The launched Orchestrator
+        string: The IP address and port used to access the Orchestrator
+        bool: Flag to indicate whether Orchestrator is clustered.
 
-  # Generate flexi experiment
-  exp = Experiment("flexi", launcher=launcher_type)
+    Note:
+        Admissable combinations of Experiment launcher and orchestrator type:
+            * laun.: local, orch.: pbs = incompatible.
+            * laun.: local, orch.: local = only 1 in-memory database possible.
+                `mpirun` will still distribute the flexi instances to other
+                nodes.
+            * laun.: pbs, orch.: pbs = does not support clusters of size 2
+                otherwise works flawlessly (warning: orchestrator doesn't find
+                the cluster configuration).
+            * laun.: pbs, orch.: local = not supported error: not supported by
+                PBSPro.
 
+    TODO:
+        * Add support for SLURM.
+        * Clean implementation and nesting.
+        * Make object out of this.
+        * Allow to reconnect to already started Orchestrator
+        * Or closue Orchestrator still open from previous run
+    """
 
-  # Initialize the orchestrator based on the orchestrator_type
-  if orchestrator_type.casefold() == "local":
-    db = Orchestrator(
+    rlxout.small_banner('Starting SmartSim...')
+
+    # Check whether launcher and orchestrator are identical (case-insensitive)
+    if not launcher_type.casefold() == orchestrator_type.casefold():
+        rlxout.warning(f'Chosen Launcher {launcher_type} and orchestrator {orchestrator_type} are incompatible! Please choose identical types for both!')
+
+    # Is database clustered, i.e. hosted on different nodes?
+    db_is_clustered = num_dbs > 1
+
+    # First try PBS if necessary. Use local configuration as backup
+    pbs_failed = False
+    if launcher_type.casefold() == 'pbs':
+        try:
+            # try to load the batch settings from the batch job environment
+            # variables like PBS_JOBID and PBS_NODEFILE
+            walltime = get_pbs_walltime()
+            hosts = get_pbs_hosts()
+            num_hosts = len(hosts)
+            rlxout.info(f"Identified available nodes: {hosts}")
+
+            # Maximum of 1 DB per node allowed for PBS Orchestrator
+            if num_hosts < num_dbs:
+                rlxout.warning(f"You selected {num_dbs} databases and {num_hosts} nodes, but maximum is 1 database per node. Setting number of databases to {num_hosts}")
+                num_dbs = num_hosts
+
+            # Clustered DB with PBS orchestrator requires at least 3 nodes for reasons
+            if db_is_clustered:
+                if num_dbs < 3:
+                    rlxout.warning(f"Only {num_dbs} databases requested, but clustered orchestrator requires 3 or more databases. Non-clustered orchestrator is launched instead!")
+                    db_is_clustered = False
+                else:
+                    rlxout.info(f"Using a clustered database with {num_dbs} instances.")
+            else:
+                rlxout.info("Using an UNclustered database on root node.")
+
+        except RuntimeError:
+            # If no env. variables for batchjob, use the local launcher
+            rlxout.warning("Didn't find pbs batch environment. Switching to local setup.")
+            pbs_failed = True
+
+    # If local configuration is required or if scheduler-based launcher failed.
+    if (launcher_type.casefold() == 'local') or pbs_failed:
+        launcher_type = "local"
+        orchestrator_type = "local"
+        db_is_clustered = False
+        hosts = [get_host()]
+
+    # Generate flexi experiment
+    exp = Experiment("flexi", launcher=launcher_type)
+
+    # Initialize the orchestrator based on the orchestrator_type
+    if orchestrator_type.casefold() == "local":
+        db = Orchestrator(
             port=port,
             interface='lo'
-            )
+        )
 
-  elif orchestrator_type.casefold() =="pbs":
-    db = Orchestrator(
+    elif orchestrator_type.casefold() == "pbs":
+        db = Orchestrator(
             launcher='pbs',
             port=port,
             db_nodes=num_dbs,
-            batch=False, # false if it is launched in an interactive batch job 
-            time=walltime, # this is necessary, otherwise the orchestrator wont run properly
-            interface=NETWORK_INTERFACE,
-            hosts=hosts, # this must be the hostnames of the nodes, it mustn't be the ip-addresses
+            batch=False,  # false if it is launched in an interactive batch job
+            time=walltime,  # this is necessary, otherwise the orchestrator wont run properly
+            interface=network_interface,
+            hosts=hosts,  # this must be the hostnames of the nodes, it mustn't be the ip-addresses
             run_command="mpirun"
-            )
-  else:
-    rlxout.warning("Orchester type "+orchestrator_type+" not implemented")
+        )
+    else:
+        rlxout.warning(f"Orchester type {orchestrator_type} not implemented!")
+        raise NotImplementedError
 
+    # startup Orchestrator
+    rlxout.info("Starting the Database...", newline=False)
+    exp.start(db)
 
-  # remove db files from previous run if necessary
-  #if CLEAN_PREVIOUS_RUN:
-  #  db.remove_stale_files()
+    # get the database nodes and select the first one
+    entry_db = socket.gethostbyname(db.hosts[0])
+    rlxout.info(f"Identified 1 of {len(db.hosts)} database hosts to later connect clients to: {entry_db}", newline=False)
+    rlxout.info("If the SmartRedis database isn't stopping properly you can use this command to stop it from the command line:")
+    for db_host in db.hosts:
+        rlxout.info(f"$(smart dbcli) -h {db_host} -p {port} shutdown", newline=False)
 
-  # startup Orchestrator
-  rlxout.info("Starting the Database...",newline=False)
-  exp.start(db)
+    # If multiple nodes are available, the first executes Relexi, while
+    # all worker processes are started on different nodes.
+    if len(hosts) > 1:
+        worker_nodes = hosts[1:]
+    else:  # Only single node
+        worker_nodes = hosts
 
-  # get the database nodes and select the first one
-  entry_db = socket.gethostbyname(db.hosts[0])
-  rlxout.info(f"Identified 1 of {len(db.hosts)} database hosts to later connect clients to: {entry_db}",newline=False)
-  rlxout.info(f"If the SmartRedis database isn't stopping properly you can use this command to stop it from the command line:")
-  for db_host in db.hosts:
-    rlxout.info(f"$(smart --dbcli) -h {db_host} -p {port} shutdown",newline=False)
-
-
-  # If multiple nodes are available, the first executes ReLeXI, while 
-  # all worker processes are started on different nodes.
-  if len(hosts)>1:
-    worker_nodes = hosts[1:]
-  else: # Only single node
-    worker_nodes = hosts
-
-
-  return exp, worker_nodes, db, entry_db, db_is_clustered
+    return exp, worker_nodes, db, entry_db, db_is_clustered
