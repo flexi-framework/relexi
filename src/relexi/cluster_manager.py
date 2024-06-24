6a008d510da2513f2d2d78c5e1323179979e1cf4
Marius Kurz <m.kurz@cwi.nl>
2024-06-24 14:37:20 +0200

    Improve Docstrings for ClusterManager, explicit typing and consistent use of "Distributed Mode" and "Local Mode".

--- src/relexi/cluster_manager.py
+++ src/relexi/cluster_manager.py
@@ -1,29 +1,45 @@
 #!/usr/bin/env python3
 
-"""The Cluster Manager contains all information about the HPC environment."""
+"""The ClusterManager class for managing the HPC environment."""
 
 import os
-import sys
 import socket
+from typing import List, Optional
 
 import smartsim
 
 import relexi.io.output as rlxout
 
 
-class ClusterManager():
-    """Base class for cluster managers.
+class ClusterManager:
+    """Class containing information about and handling the HPC environment.
 
-    This class defines the interface for cluster managers. which contain all
-    information of the HPC environment used for the training environments. In
-    particular, it provides properties to access the hostnames and entails the
-    launched Orchestrator.
+    This class defines the interface for cluster managers, which contain all
+    information of the HPC environment used for the training environments and
+    method to manage it. This include in particular to identify the scheduler
+    environment, identify the hostnames of the available nodes as well as
+    launching and managing the SmartSim experiment including the Orchestrator.
 
-    Properties:
-        type (str): Type of the cluster manager.
-        hosts (list): List of hostnames.
-        dedicated_head (bool): Whether dedicated head and worker nodes are
+    Two possible modes are available for using the available compute resources:
+
+        - **Distributed Mode**: The localhost running the main training script
+            becomes the dedicated Head node that hosts the database and runs
+            the model evaluation and training loop. All training environments
+            are distributed to the available Worker nodes.
+
+        - **Local Mode**: The training script, the database and the training
+            environments are all placed on the localhost.
+
+    For **Distributed Mode**, more than 1 node has to be available. Otherwise
+    **Local Mode** will be used. The mode of the `ClusterManager` can be
+    retrieved via the `is_distributed` attribute.
+
+    Attributes:
+        type (str): Type of the cluster manager. Must be one of
+            **{'local', 'pbs', 'slurm'}**.
+        is_distributed (bool): Whether dedicated head and worker nodes are
             available or everything runs on single shared node.
+        hosts (list): List of hostnames.
         head (str): Hostname of Head node.
         workers (list): List of worker nodes.
 
@@ -32,47 +48,58 @@ class ClusterManager():
         generate_rankfiles: Generate rank files for OpenMPI process binding.
 
     Raises:
+        ValueError: If the scheduler type is not supported.
+        RuntimeError: If the following conditions are met:
+            - The scheduler environment cannot be identified, or
+            - Launching the Orchestrator fails.
         NotImplementedError: If the methods are not implemented for the
             provided scheduler type.
-        ValueError: If the scheduler type is not supported.
     """
 
     TYPES = ['local', 'pbs', 'slurm']  # All implemented types of cluster managers
 
     def __init__(
             self,
-            scheduler_type='local',
-            db_port=6790,
-            db_network_interface='lo',
+            scheduler_type: Optional[str] = 'local',
+            db_port: Optional[int] = 6790,
+            db_network_interface: Optional[str] = 'lo'
             ):
-
-        # Check if the scheduler type is supported
         self.type = scheduler_type.casefold().strip()
-
         rlxout.info(f'Trying to identify {self.type} training environment...')
+
         try:
             self._hosts = self._get_hostlist()
         except Exception as e:
-            if self.type == 'local':
-                raise RuntimeError('Failed to setup local training environment!')
             rlxout.warning(f'Failed: {e}')
-            rlxout.info('Trying to run in local mode instead...', newline=False)
-            try:
-                self.type = 'local'
-                self._hosts = self._get_hostlist()
-            except:
-                raise RuntimeError('Also failed to setup local environment!')
+            if self.type != 'local':
+                rlxout.info('Trying to run in local mode instead...', newline=False)
+                try:
+                    self.type = 'local'
+                    self._hosts = self._get_hostlist()
+                except Exception as e:
+                    raise RuntimeError('Also failed to setup local environment!') from e
+            else:
+                raise RuntimeError('Failed to setup local training environment!') from e
         rlxout.info('Success!', newline=False)
 
         self.db = None
-        self.exp, self.db, self.entry_db = self._launch_orchestrator(port=db_port,
-                                                      network_interface=db_network_interface)
+        self.exp, self.db, self.entry_db = self._launch_orchestrator(
+            port=db_port,
+            network_interface=db_network_interface,
+        )
 
     def __del__(self):
         if self.db:
-            self.exp.stop(self.db)
+            try:
+                self.exp.stop(self.db)
+            except Exception as e:
+                raise RuntimeError('Failed to stop the Orchestrator!') from e
 
-    def _launch_orchestrator(self, port, network_interface):
+    def _launch_orchestrator(
+            self,
+            port: int,
+            network_interface: str
+            ) -> tuple[smartsim.Experiment, smartsim.Database, str]:
         """Launches the SmartSim Orchestrator for the current job.
 
         Args:
@@ -80,8 +107,7 @@ class ClusterManager():
             network_interface (str): Network interface to use for the Orchestrator.
 
         Returns:
-            Experiment: The Experiment object.
-            Orchestrator: The Orchestrator object.
+            tuple: The Experiment object, the Orchestrator object, and the entry database hostname.
         """
         rlxout.small_banner('Starting Orchestrator...')
 
@@ -89,22 +115,19 @@ class ClusterManager():
         exp = smartsim.Experiment('flexi', launcher=self.type)
 
         # Initialize the orchestrator based on the orchestrator_type
-        if self.type == 'local':
-            db = exp.create_database(port=port, interface='lo')
-        elif self.type in {'pbs','slurm'}:
-            db = exp.create_database(hosts=self.head, port=port, interface=network_interface)
-        else:
-            raise NotImplementedError(f"Orchestrator type {self.type} not implemented.")
+        db = exp.create_database(
+            port=port,
+            interface='lo' if self.type == 'local' else network_interface,
+            hosts=self.head if self.type in {'pbs', 'slurm'} else None,
+        )
 
-        # startup Orchestrator
         rlxout.info("Starting the Database...", newline=False)
         try:
             exp.start(db)
         except Exception as e:
-            raise RuntimeError(f"Failed to start the Orchestrator: {e}")
+            raise RuntimeError(f"Failed to start the Orchestrator: {e}") from e
         rlxout.info("Success!", newline=False)
 
-        # get the database nodes and select the first one
         entry_db = socket.gethostbyname(db.hosts[0])
         rlxout.info("If the SmartRedis database isn't stopping properly you can use this command to stop it from the command line:")
         rlxout.info(f"$(smart dbcli) -h {db.hosts[0]} -p {port} shutdown", newline=False)
@@ -112,21 +135,25 @@ class ClusterManager():
         return exp, db, entry_db
 
     def info(self):
-        """Print information about the current job."""
-        rlxout.info("Found the following environment information:")
-        rlxout.info(f"Scheduler: {self.type}",    newline=False)
-        rlxout.info(f"Hosts:     {self.hosts}",   newline=False)
-        rlxout.info(f"Head node: {self.head}",    newline=False)
-        rlxout.info(f"Workers:   {self.workers}", newline=False)
+        """Print information about the current environment."""
+        rlxout.info("Found the following environment:")
+        rlxout.info(f"  Scheduler: {self.type}", newline=False)
+        rlxout.info(f"  Hosts:     {self.hosts}", newline=False)
+        if self.is_distributed:
+            rlxout.info(f"Relexi is running in distributed mode:")
+            rlxout.info(f"  Head node: {self.head}", newline=False)
+            rlxout.info(f"  Workers:   {self.workers}", newline=False)
+        else:
+            rlxout.info(f"Relexi is running in local mode on: {self.head}")
 
-    def _get_hostlist(self):
+    def _get_hostlist(self) -> List[str]:
         """Get the list of hosts the script is executed on.
 
         Uses the scheduler type to determine the hostlist via the environment
         variables set by the scheduler.
 
         Returns:
-            List containing the hostnames as strings.
+            list: List containing the hostnames as strings.
 
         Raises:
             NotImplementedError: If the method is not implemented for the
@@ -142,85 +169,80 @@ class ClusterManager():
             raise NotImplementedError(f"Method get_hostlist not implemented for scheduler type {self.type}")
 
     @property
-    def type(self):
+    def type(self) -> str:
         """Get the type of scheduler environment used for the cluster manager.
 
         Returns:
-            Type of the cluster manager as a string.
+            str: Type of the cluster manager.
         """
         return self._type
 
     @type.setter
-    def type(self, value):
+    def type(self, value: str):
         """Set the type of scheduler environment used for the cluster manager.
         Ensure that the type is supported.
 
         Args:
-            value (str): Type of the cluster manager as a string.
+            value (str): Type of the cluster manager.
         """
-        if not value in self.TYPES:
+        if value not in self.TYPES:
             raise ValueError(f"Scheduler type {value} not supported.")
         self._type = value
 
     @property
-    def hosts(self):
+    def hosts(self) -> List[str]:
         """Get the list of hosts the script is executed on.
 
         Returns:
-            List containing the hostnames as strings.
+            list: List containing the hostnames as strings.
         """
         return self._hosts
 
     @property
-    def dedicated_head(self):
-        """Return whether dedicated head and worker nodes are available or
-        everything runs on single shared node.
+    def is_distributed(self) -> bool:
+        """Returns whether ClusterManager runs in distributed or local mode.
 
         Returns:
-            Bool indicating whether dedicated head is used or not.
+            bool: Indicates whether cluster runs in distributed or local mode.
+                - True: A dedicated head node is used for training and the
+                    database and at least one additional worker node to run
+                    simulations.
+                - False: Only single machine is available and training,
+                    database and simulation will be performed on the localhost.
         """
         return len(self._hosts) > 1
 
     @property
-    def head(self):
-        """Get head node, which is first node, i.e. node Relexi is actually
-        started on.
+    def head(self) -> str:
+        """Get head node, which is where Relexi is actually runs on.
 
         Returns:
-            Hostname of Head node as string.
+            str: Hostname of the head node.
         """
-        return self._hosts[0]
+        return self.hosts[0]
 
     @property
-    def workers(self):
-        """Get a list of worker nodes.
+    def workers(self) -> List[str]:
+        """Get a list of worker nodes depending on mode.
 
         Returns:
-            List containing the hostnames of worker nodes as strings.
+            list: List containing the hostnames of worker nodes as strings.
         """
-        if self.dedicated_head:
-            return self._hosts[1:]
-        return self._hosts
+        if self.is_distributed:
+            return self.hosts[1:]
+        return self.hosts
 
-    def generate_rankfiles(self, n_models, n_ranks_per_model, base_path=None):
+    def generate_rankfiles(self, n_models: int, n_ranks_per_model: int, base_path: Optional[str] = None) -> List[str]:
         """Generate rank file for OpenMPI process binding.
 
         Args:
             n_models (int): Number of models to be launched.
             n_ranks_per_model (int): Number of ranks used for each model.
-            base_path (str): (Optional.) Path to the directory of the rank files.
+            base_path (str, optional): Path to the directory of the rank files.
 
         Returns:
-            List of filenames of the rankfiles.
+            list: List of filenames of the rankfiles.
         """
-        if self.type == 'local':
-            return generate_rankfile_ompi(self.hosts, cores_per_node, n_par_env, ranks_per_env, base_path)
-        if self.type == 'pbs':
-            return generate_rankfile_ompi(self.hosts, cores_per_node, n_par_env, ranks_per_env, base_path)
-        if self.type == 'slurm':
-            return generate_rankfile_ompi(self.hosts, cores_per_node, n_par_env, ranks_per_env, base_path)
-        else:
+        if self.type not in self.TYPES:
             raise NotImplementedError(f"Method generate_rankfile not implemented for scheduler type {self.type}")
-
-if __name__ == '__main__':
-    db = ClusterManager()
+        return generate_rankfile_ompi(self.hosts, n_models, n_ranks_per_model, base_path)
