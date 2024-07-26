362c0a292626f482c45316f236f42b758c032e92
Marius Kurz <marius.kurz@iag.uni-stuttgart.de>
2024-07-26 09:49:03 +0000

    Relexi as Python package.

--- tests/context.py
+++ tests/context.py
@@ -1,5 +1,5 @@
 # Import Relexi package to path for testing
 import os
 import sys
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__),'../relexi')))
 import relexi
