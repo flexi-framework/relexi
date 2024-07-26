26c501075eb6e80712f647c6936523402aac9bad
Marius Kurz <marius.kurz@iag.uni-stuttgart.de>
2024-07-26 11:04:12 +0000

    Add licensing header to all files containing the encoding, the SPDX identifier and the licensing stub with copyright holders.

---
+++ relexi/rl/__init__.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+# This file is part of Relexi, a reinforcement learning framework for training
+# machine learning models in simulations on high-performance computing systems.
+#
+# Copyright (c) 2022-2024 Marius Kurz, Andrea Beck
+#
+# Relexi is free software: you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# Relexi is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# Relexi. If not, see <http://www.gnu.org/licenses/>.
